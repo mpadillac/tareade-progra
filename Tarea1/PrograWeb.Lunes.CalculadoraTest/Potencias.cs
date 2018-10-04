42b72c6733bde36546f0e35cba6a47fa2de2bf97
Mayron Padilla Castillo <m4yronpadilla@gmail.com>
2018-10-03 23:35:44 -0600

    Test

---
+++ Tarea1/PrograWeb.Lunes.CalculadoraTest/Potencias.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrograWeb.Lunes.ServicioCalculadora;
+
+namespace PrograWeb.Lunes.CalculadoraTest
+{
+    [TestClass]
+    public class Potencias
+    {
+        [TestMethod]
+        public void BasePositivaPotenciaPositiva()
+        {
+            // preparación del escenario
+            var miBase = 5.0;
+            var miPotencia = 3.0;
+            var miValorEsperado = 125.0;
+            var miValorReal = 0.0;
+
+            // invocamos el método
+            // para invocar dinámicamente al método
+            var elServicio = new ServicioCalculadora.Service1();
+            miValorReal = elServicio.Potencia(miBase, miPotencia);
+
+            // verificar el resultado obtenido
+            Assert.AreEqual(miValorEsperado, miValorReal);
+
+
+
+
+        }
+    }
+}
---
+++ Tarea1/PrograWeb.Lunes.CalculadoraTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrograWeb.Lunes.CalculadoraTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrograWeb.Lunes.CalculadoraTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a622603b-c389-4d72-a333-d74697b7073b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
