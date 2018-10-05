26c92b8c8524c56d0adf45cdb3efdc4f43a1de5d
Mayron Padilla Castillo <m4yronpadilla@gmail.com>
2018-10-04 22:11:26 -0600

    validación

---
+++ Tarea1/PrograWeb.Lunes.ServicioCalculadora/Logica/Validacion/OperacionCuadratica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrograWeb.Lunes.ServicioCalculadora.Logica.Validacion
+{
+    public class OperacionCuadratica
+    {
+        public bool LosOperadoresSonCorrectos(double a, double b, double c)
+        {
+            bool Result = true;
+            // se validaque A sea diferente de 0
+
+            Result = !(a == 0);
+            return Result;
+        }
+    }
+}
